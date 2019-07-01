24f1459bf495b6979a3b91782b52b568c2bfd140
Ochoa <ochoaw@wit.edu>
2019-07-01 14:05:31 -0400

    Added simplified functions for stuff

--- src/user.cpp
+++ src/user.cpp
@@ -50,23 +50,48 @@ void student::courseRegister(){
         else{
             cout << "Please enter the CRN you wish to add." << endl;
             cin >> crnInput;
-            for(int i = regCourses.begin(); i != regCourse.end(); i++){
-                if(i == crnInput){
-                    regCourses.erase();
-                    cout << "Course has been dropped." << endl;
-                    return;
-                }   
-            }
-            cout << "" << endl;
+            regCourse.pushback(crnInput);
+            cout << "You have been enrolled in the course." << endl;
         }
     }
     else if(dropAdd == 2){
-        
+      if(student->regCourses.size() == 0){
+        cout << "Sorry, you have not registered for any courses yet." << endl;
+        return;
+      }
+      else{
+        cout << "Please enter the CRN you wish to remove." << endl;
+        cin >> crnInput;
+        for(int i = regCourses.begin(); i != regCourses.end(); i++){
+          if(i == crnInput){
+              regCourses.erase();
+              cout << "Course has been dropped." << endl;
+              return;
+          }   
+        }
+      }
+    }
+    else{
+      cout << "Sorry, that was not one of the options." << endl;
     }
 }
 //Wilson
 void instructor::checkRoster(){
-
+  int crnInput;
+  cout << "Please enter the CRN for the course roster you wish to view." << endl;
+  cin >> crnInput;
+  for(int i = instructor->regCourses.begin(); i != instructor->regCourses.end(); i++){
+    if(i == crnInput){
+      cout << "The students in this course are :" << endl;
+      for(int j = 0; j < studentsinCRN.size(); j++){
+        cout << studentsinCRN[i] << endl;
+      }
+      cout << "~END OF COURSE LIST~" << endl;
+      return;
+    }
+  }
+  cout << "Sorry, you are not assigned to any courses with that CRN." << endl;
+  return;
 }
 
 // Julia
@@ -75,11 +100,69 @@ void instructor::checkCourses(){
 }
 //Wilson
 void admin::checkRoster(){
-// Wilson
+  int crnInput;
+  cout << "Please enter the CRN for the course roster you wish to view." << endl;
+  cin >> crnInput;
+  for(int i = allCourses.begin(); i != allCourses.end(); i++){
+    if(i == crnInput){
+    cout << "The students in this course are :" << endl;
+      for(int j = 0; j < studentsinCRN.size(); j++){
+        cout << studentsinCRN[i] << endl;
+      }
+      cout << "~END OF COURSE LIST~" << endl;
+      return;
+    }
+  }
+  cout << "Sorry, no course was found with that CRN." << endl;  
 }
-
+// Wilson
 void admin::editCourse(){
-
+  int adminChoice, crnInput;
+  string courseName, courseDays;
+  cout << "Type 1 to add a new course, 2 to remove a course, and 3 to edit an existing course." << endl;
+  cin >> adminChoice;
+  switch(adminChoice){
+    case 1:
+      cout << "Please type the CRN, Course Name, and days of the course you wish to add." << endl;
+      cin >> crnInput >> courseName >> courseDays;
+      for(int i = allCourses.begin(); i != allCourses.end(); i++){
+        if(i == crnInput){
+          cout << "Sorry, a course has already been created with that unique identifier." << endl;
+          break;
+        }
+      }
+      allCourses.pushback(crnInput);
+      allCourses.back()->cName = courseName;
+      allCourses.back()->cDays = courseDays;
+      cout << "The course has been created." << endl;
+      break;
+    case 2:
+      cout << "Please enter the CRN you wish to remove." << endl;
+        cin >> crnInput;
+        for(int i = allCourses.begin(); i != allCourses.end(); i++){
+          if(i == crnInput){
+              allCourses.erase();
+              cout << "Course has been dropped." << endl;
+              break;
+          }   
+        }
+        cout << "Sorry, a course was not found with that CRN." << endl;
+        break;
+    case 3:
+       cout << "Please enter the CRN you wish to edit." << endl;
+        cin >> crnInput;
+        for(int i = allCourses.begin(); i != allCourses.end(); i++){
+          if(i == crnInput){
+            cout << "What would you like to edit?" << endl;
+            break;
+          }
+        }
+        cout << "Sorry, a course was not found with that CRN." << endl;
+        break;
+    default:
+      cout << "Sorry, that was not one of the options." << endl;
+      break;
+  }
 }
 
 // Julia

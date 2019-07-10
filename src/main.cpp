a5a48ad0c34aece3cdce222dfd5415f84899e7a9
Ochoa <ochoaw@wit.edu>
2019-07-10 13:56:25 -0400

    Added Main Menu for each user

--- src/main.cpp
+++ src/main.cpp
@@ -10,38 +10,95 @@ list<course> courses;
 int main(){
   int choice = 0;
   string userEmail, userPW;
-  string 
   string userRole;
+  int quitter
+  int loggedIn = 0;
   do{
     cout << "-------------LOGIN MENU--------------" << ENDL;
     cout << "Enter email: ";
     cin >> userEmail;
-    cout << "Enter password: ";
+    cout << endl << "Enter password: ";
     cin << userPW;
     list<user>::iterator i = users.begin();
     for(i;i!=user.end();i++){
       (i).login(users);
     }
-    userRole = user->role;
-    userID = user->userID;
-    if(userRole == "admin"){
-      
+
+    currentUser.userRole = user->role;
+    currentUser.userID = user->userID;
+    loggedIn = 1;
+    if(currentUser.userRole == "admin"){
+      while(loggedIn == 1){  
+        cout << endl << "---------------ADMIN MENU----------------" << endl;
+        cout << endl << "Type 1 to check a roster, type 2 to edit a course, type 3 to edit a student, or type 9 to Logout." << endl;
+        switch(choice){
+          case 1:
+            currentUser.checkRoster();
+            break;
+          case 2:
+            currentUser.editCourse();
+            break;
+          case 3:
+            currentUser.editStudent();
+            break;
+          case 9:
+            currentUser.logout();
+            loggedIn = 0;
+            currentUser.userRole = NULL;
+            break;
+          default:
+            cout << "Sorry, that was not one of the choices. Please try again." << endl;
+            break;
+        }
+      }
     }
-    else if(userRole == "student"){
-    
+    else if(currentUser.userRole == "student"){
+      while(loggedIn == 1){
+        cout << endl << "---------------STUDENT MENU----------------" << endl;
+        cout << endl << "Type 1 to enter course registration, type 2 to check your schedule, or type 9 to Logout." << endl;
+        switch(choice){
+          case 1:
+            currentUser.courseRegister();
+            break;
+          case 2:
+            currentUser.checkSchedule();
+            break;
+          case 9:
+            currentUser.logout();
+            loggedIn = 0;
+            currentUser.userRole = NULL;
+            break;
+          default:
+            cout << "Sorry, that was not one of the choices. Please try again." << endl;
+            break;
+        }
+      }
     }
-    else if(userRole == "instructor"){
-    
+    else if(currentUser.userRole == "instructor"){
+      while(loggedIn == 1){
+          cout << endl << "---------------INSTRUCTOR MENU----------------" << endl;
+        cout << endl << "Type 1 to check a roster, type 2 to check your courses, or type 9 to Logout." << endl;
+        switch(choice){
+          case 1:
+            currentUser.checkRoster();
+            break;
+          case 2:
+            currentUser.checkCourses();
+            break;
+          case 9:
+            currentUser.logout();
+            loggedIn = 0;
+            currentUser.userRole = NULL;
+            break;
+          default:
+            cout << "Sorry, that was not one of the choices. Please try again." << endl;
+            break;
+        }
+      }
     }
     else{
-
+      cout << "Sorry, it seems you are an unauthorized user. Please check with your Administrator to gain privileges." << endl;
+      break;
     }
-    cout << endl << "---------------MAIN MENU----------------" << endl;
-    cout << endl << "Type 1 to check your roster, type 2 to "
-    switch(choice){
-      case 0:
-      if
-
-    }
-  } while ();
+  } while (quitter == 0);
 }
